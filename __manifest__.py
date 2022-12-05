7ebebf069eb9d8f2b4d2b5aee0f51f18f401aa49
ChaoqiYin <305111632@qq.com>
2022-12-05 17:09:14 +0800

    build: 2.0.0

--- __manifest__.py
+++ __manifest__.py
@@ -14,14 +14,14 @@
     'website': "https://www.funenc.com",
 
     'external_dependencies': {
-        'python': ['aiohttp'],
+        'python': ['aiohttp', 'Crypto'],
     },
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '1.0.0',
+    'version': '2.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'hr'],
